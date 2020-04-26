dee6dcec6a47d8bece129737efba2d7aaef3f1de
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-04-26 23:01:05 +0530

    Add files via upload

---
+++ File_Hanling/file handling.py
@@ -0,0 +1,11 @@
+
+# open file
+f = open("input00.txt")
+
+# open image for read binary mode
+f = open("image.bmp",'r+b')
+
+# use defalt encoding 'utf-8'
+# if we working file with text mode encoding type is highly recomend
+
+f = open("test.txt",mode = 'r', encodnig = 'utf-8')

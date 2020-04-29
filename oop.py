8eb7e3b595baf6168fadcd71e38f7952e90ab1b3
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-04-29 11:28:43 +0530

    Add files via upload

--- oop.py
+++ oop.py
@@ -80,7 +80,7 @@ class Student1:
     def __init__(self,name,rollno):
          self.name = name
          self.rollno = rollno
-         self.lap = self.laptop()  # make class objector in construcor
+         self.lap = self.Laptop()  # make class objector in construcor
          
          
     def show(self):
@@ -93,12 +93,85 @@ class Student1:
                 self.cpu = "i7"
                 self.ram = 8 
                 
-t1 = Student("Rohan",98123)
-t2 = Student("Ravindi",98615)
+t1 = Student1("Rohan",98123)
+t2 = Student1("Ravindi",98615)
 
 lap1 = t1.lap
 lap2 = t2.lap
 
 
-          
-          
+#inheritance in python
+
+class A:  #1st class(Super class)
+    def __init__(self):
+        print("A init")
+    def method1(self):
+        print("This is method 1")
+    def method2(self):
+        print("This is method 2")
+    def method3(self):
+        print("This is method 3")
+    def methodd(self):
+            print("This is class D but in A")
+
+#singal class inheritance
+#one super class & one sub class
+# for inharite class we use with in () we use class name tharefor we can inharite uper class from down class 
+class B(A): # 2nd class
+    def method4(self):
+        print("This is method 4")
+    def method5(self):
+        print("This is method 5")
+    def method6(self):
+        print("This is method 6")
+
+#now we can use uper class method form class B object
+
+obj1 = B()
+obj1.method1()
+
+#multi level inharitance
+#one supper class and 2 sub class
+class C(B):  #3rd class(Super class)
+    def __init__(self):
+        print("class C constructr")
+    
+    def method7(self):
+        print("This is method 7")
+    def method8(self):
+        print("This is method 8")
+    def method9(self):
+        print("This is method 9")
+        
+obj2 = C()
+obj2.method2()
+
+#multipule inheritance you can use more class in this method like this
+
+class D:
+    def __init__(self):
+        print("This is class D constructer")
+    
+    def methodd(self):
+        print("This is class D")
+        
+        
+class F:
+    def __init__(self):
+        print("This is class F constructrer")
+        
+    def methodf(self):
+        print("this is clss F")
+        
+class I(C,D,F): # multiple inharitance use like this
+    def __init__(self):
+        super().__init__() # hear i call class supper class constructor but hear, have 3 supper class what int can be call?
+        print("in F in init")
+    # it's call from left to right
+    #fristly call constructer in c then call D 
+    #it's called Method Resolution Order(MRO)
+    
+
+obji = I()
+
+

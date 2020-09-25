8388659769a41a29de502417558644c4690a1cc0
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-09-25 17:32:45 +0530

    c1

---
+++ jeson.py
@@ -0,0 +1,18 @@
+import json
+
+data = """
+
+"name" : "Chuck",
+"phone" : {
+    "type" : "intl",
+    "number" : "0779395415"
+},
+"email" : {
+    "hide" : "yes"
+}
+
+"""
+
+info = json.loads(data)
+print('Name:', info["name"])
+print('Hide', info["email"]["hide"])

f6ee8b13faaf86b0cd2bc6489446701e4b3e7d3b
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-09-27 13:45:31 +0530

    Update objects.py
    c3

--- objects.py
+++ objects.py
@@ -8,9 +8,12 @@ class PartyAnimal:
         self.x = self.x + 1
         print('So far', self.x)
     
+    ## This methods runs end of the class 
     def __del__(self):
         print('I am destructed',self.x)
 
 an = PartyAnimal()
+an.party()
+an.party()
 an.party()
 an.party()

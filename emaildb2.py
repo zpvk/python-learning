6eb9c08ccab9327748b3f5091d8f94874cc23acd
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-09-28 23:35:00 +0530

    c7

--- emaildb2.py
+++ emaildb2.py
@@ -2,7 +2,7 @@
 # @Author: Rohan Kumara
 # @Date:   2020-09-28 23:18:52
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-09-28 23:20:29
+# @Last Modified time: 2020-09-28 23:34:39
 
 
 import sqlite3
@@ -10,4 +10,17 @@ import sqlite3
 con = sqlite3.connect('emaildb.sqlite')
 cur = con.cursor()
 
-cur.execute('DROP TABLE IF EXISTS ')
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+
+## input data to database
+fname = input('Enter file name:')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startwith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    cur.execute('')
+    

5e22c21cf68586bf14b45728906a9ddc2a2ed8d6
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-09-28 23:13:39 +0530

    Update emaildb2.py

--- emaildb2.py
+++ emaildb2.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Rohan
+# @Date:   2020-09-28 23:11:53
+# @Last Modified by:   Your name
+# @Last Modified time: 2020-09-28 23:12:53
+
+
 import sqlite3
 
 con = sqlite3.connect('emaildb.sqlite')

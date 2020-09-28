549fa15975822203021b34ded559cc2a4ca78149
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-09-28 23:21:18 +0530

    Header update

--- emaildb.py
+++ emaildb.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Rohan Kumara
+# @Date:   2020-09-28 23:19:06
+# @Last Modified by:   Rohan Kumara
+# @Last Modified time: 2020-09-28 23:19:15
+
+
 import sqlite3
 
 conn = sqlite3.connect('emaildb.sqlite')
--- emaildb2.py
+++ emaildb2.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-# @Author: Rohan
-# @Date:   2020-09-28 23:11:53
-# @Last Modified by:   Your name
-# @Last Modified time: 2020-09-28 23:12:53
+# @Author: Rohan Kumara
+# @Date:   2020-09-28 23:18:52
+# @Last Modified by:   Rohan Kumara
+# @Last Modified time: 2020-09-28 23:20:29
 
 
 import sqlite3
 
 con = sqlite3.connect('emaildb.sqlite')
 cur = con.cursor()
+
+cur.execute('DROP TABLE IF EXISTS ')

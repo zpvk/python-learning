91d8b8e94ef165911a573634b6a74b3eeffbf785
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-09-28 21:24:28 +0530

    Update emaildb.py

--- emaildb.py
+++ emaildb.py
@@ -8,3 +8,6 @@ cur.execute('DROP TABLE IF EXISTS Counts')
 cur.execute('CREATE TABLE Counts (email TEXT, count INTEGER)')
 
 fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:

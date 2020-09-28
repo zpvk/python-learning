2e7620b5a94dedff79bf8b4a898662a5acb8efb4
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-09-29 00:59:08 +0530

    db

--- emaildb2.py
+++ emaildb2.py
@@ -2,13 +2,13 @@
 # @Author: Rohan Kumara
 # @Date:   2020-09-28 23:18:52
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-09-29 00:32:47
+# @Last Modified time: 2020-09-29 00:54:10
 
 
 import sqlite3
 
-con = sqlite3.connect('emaildb.sqlite')
-cur = con.cursor()
+conn = sqlite3.connect('emaildb.sqlite.sqlite')
+cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Counts')
 
@@ -19,19 +19,19 @@ fname = input('Enter file name:')
 if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
-    if not line.startwith('From: '): continue
+    if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE org = ? ', (email,)')
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (email,))
     row = cur.fetchone()
-    if row in None :
+    if row is None :
         cur.execute('INSERT INTO Counts (org, count) VALUES (?, 1)',(email,))
     else:
         cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (email,))
     conn.commit()
     
 #https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 100'
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])

16036828aaa1077737cebff9e9d7e1c40d81689a
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-02 03:09:32 +0530

    Update track.py

--- Track/track.py
+++ Track/track.py
@@ -6,3 +6,11 @@ cur = conn.cursor()
 
 # Make some fresh tables using executescript()
 cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+
+CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);

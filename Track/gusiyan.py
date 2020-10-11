83dd9c5b2e140a81d26ddd856e42a534c1997a0d
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-11 19:52:42 +0530

    gwsian

---
+++ Track/gusiyan.py
@@ -0,0 +1,12 @@
+import math
+import matplotlib.pyplot as plt
+
+class Gaussian():
+    """ Gaussian distribution class for calculating and 
+    visualizing a Gaussian distribution.
+    
+    Attributes:
+        mean (float) representing the mean value of the distribution
+        stdev (float) representing the standard deviation of the distribution
+        data_list (list of floats) a list of floats extracted from the data file
+            

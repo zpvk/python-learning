fff054f34b825878a4004058a6558cd53ea0966e
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-11 19:54:37 +0530

    Update gusiyan.py

--- Track/gusiyan.py
+++ Track/gusiyan.py
@@ -9,4 +9,10 @@ class Gaussian():
         mean (float) representing the mean value of the distribution
         stdev (float) representing the standard deviation of the distribution
         data_list (list of floats) a list of floats extracted from the data file
-            
+                """
+    def __init__(self, mu = 0, sigma = 1):
+        
+        self.mean = mu
+        self.stdev = sigma
+        self.data = []
+

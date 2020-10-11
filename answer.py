e78a96388f825c70aef239285092024b34141241
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-11 20:30:20 +0530

    Update answer.py

--- answer.py
+++ answer.py
@@ -9,3 +9,5 @@ class Gaussian():
         mean (float) representing the mean value of the distribution
         stdev (float) representing the standard deviation of the distribution
         data_list (list of floats) a list of floats extracted from the data file
+                    
+    """

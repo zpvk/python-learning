f8642b4452f15a665d0ab316de25654784d97791
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-11 20:41:47 +0530

    Update answer.py

--- answer.py
+++ answer.py
@@ -46,5 +46,5 @@ class Gaussian():
             sample (bool): whether the data represents a sample or population
                     
         Returns: 
-        float: standard deviation of the data set
+             float: standard deviation of the data set
     

fb380509ce2a6dc5cbc34a6b70cc360e018b30c8
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-11 20:52:31 +0530

    Update answer.py

--- answer.py
+++ answer.py
@@ -102,3 +102,7 @@ class Gaussian():
     def plot_histogram(self):
                 """Function to output a histogram of the instance variable data using 
         matplotlib pyplot library.
+
+                
+        Args:
+            None

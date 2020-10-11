a299bfd252dbb0c1615ff38d3d0c6f1108def2e6
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-11 20:46:15 +0530

    Update answer.py

--- answer.py
+++ answer.py
@@ -73,3 +73,4 @@ class Gaussian():
             
         """Function to read in data from a txt file. The txt file should have
         one number (float) per line. The numbers are stored in the data attribute. 
+        After reading in the file, the mean and standard deviation are calculated

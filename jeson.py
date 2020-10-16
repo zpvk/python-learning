219d8103e46610221607f9fa361a54cd9adbd5cd
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-16 17:39:56 +0530

    Updateed jeson.py

--- jeson.py
+++ jeson.py
@@ -2,15 +2,15 @@ import json
 
 data = '''
 {
-"name" : "Chuck",
-"phone" : {
-    "type" : "intl",
-    "number" : "0779395415"
-},
-"email" : {
-    "hide" : "yes"}
-}
-'''
+    "name" : "Chuck",
+    "phone" : {
+        "type" : "intl",
+        "number" : "0779395415"
+    },
+    "email" : {
+    "hide" : "yes"
+    }
+}'''
 
 info = json.loads(data)
 print('Name:', info["name"])

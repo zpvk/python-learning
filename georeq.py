29027b0b6fa91921e461da0b9eca9264fdbe5b3c
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-21 23:52:34 +0530

    final geo

--- georeq.py
+++ georeq.py
@@ -44,4 +44,5 @@ while True:
     lng = js['results'][0]['geometry']['location']['lng']
     print('lat', lat, 'lng', lng)
     location = js['results'][0]['formatted_address']
+    print(location)
 

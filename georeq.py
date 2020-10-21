69999802301ec764610acf89371f5600cd7a6fc6
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-21 23:49:23 +0530

    ce

--- georeq.py
+++ georeq.py
@@ -6,3 +6,11 @@ api_key = False
 # If you have a Google Places API key, enter it here
 # api_key = 'AIzaSy___IDByT70'
 # https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+while True:

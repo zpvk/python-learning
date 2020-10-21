65c79b6b62a117bc3d7ff5f9814f6f97f92def53
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-21 23:49:05 +0530

    ce

--- georeq.py
+++ georeq.py
@@ -1,3 +1,8 @@
 import requests
 # You need to install the requests module to use this code
 import json
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro

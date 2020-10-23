0ebc2bbca0a526cf5e8aad51d695e4a9c781d077
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-23 22:29:57 +0530

    cs

--- math lab/matrix.m
+++ math lab/matrix.m
@@ -2,10 +2,14 @@
  * @Author: Rohan K 
  * @Date: 2020-10-23 22:22:34 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2020-10-23 22:29:25
+ * @Last Modified time: 2020-10-23 22:29:50
  */
 
 % The ; denotes we are going back to a new row.
 A = [1, 2, 3; 4, 5, 6; 7, 8, 9; 10, 11, 12]
 
 % Initialize a vector 
+v = [1;2;3]
+
+% Get the dimension of the matrix A where m = rows and n = columns
+[m,n] = size(A)

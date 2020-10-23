fdff1653fd6cbc2f3f79aa8ad2f6c39d6a35abcb
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-23 22:28:54 +0530

    cd

--- math lab/matrix.m
+++ math lab/matrix.m
@@ -1,5 +1,9 @@
-
-
+/*
+ * @Author: Rohan K 
+ * @Date: 2020-10-23 22:22:34 
+ * @Last Modified by: Rohan K
+ * @Last Modified time: 2020-10-23 22:28:33
+ */
 
 % The ; denotes we are going back to a new row.
 A = [1, 2, 3; 4, 5, 6; 7, 8, 9; 10, 11, 12]

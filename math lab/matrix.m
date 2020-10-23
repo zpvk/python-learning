93b29edc6f1333b82ec16e0f8750543dbd434708
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-23 22:29:28 +0530

    cd

--- math lab/matrix.m
+++ math lab/matrix.m
@@ -1,9 +1,11 @@
 /*
  * @Author: Rohan K 
  * @Date: 2020-10-23 22:22:34 
- * @Last Modified by: Rohan K
- * @Last Modified time: 2020-10-23 22:28:33
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2020-10-23 22:29:25
  */
 
 % The ; denotes we are going back to a new row.
 A = [1, 2, 3; 4, 5, 6; 7, 8, 9; 10, 11, 12]
+
+% Initialize a vector 

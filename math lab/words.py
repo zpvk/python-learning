fb7f6bbcd4057f3c09377f331cb3e09f4744b120
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-25 22:22:42 +0530

    ry

--- math lab/words.py
+++ math lab/words.py
@@ -2,7 +2,7 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-25 22:19:03
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-25 22:22:16
+# @Last Modified time: 2020-10-25 22:22:36
 
 
 
@@ -18,3 +18,4 @@ for line in handle:
 bigcount = None
 bigword = None
 for word, count in list(counts.items()):
+    if bigcount is None or count > bigcount:

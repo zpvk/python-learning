14e72ad0972a2474e4ecb3bd59fbd6aac6359efa
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-25 22:19:52 +0530

    ko

--- math lab/words.py
+++ math lab/words.py
@@ -2,13 +2,8 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-25 22:19:03
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-25 22:19:03
+# @Last Modified time: 2020-10-25 22:19:46
 
 
-def main():
-    print("Hello, World!")
 
-
-if __name__ == "__main__":
-    main()
 name = input('Enter file:')

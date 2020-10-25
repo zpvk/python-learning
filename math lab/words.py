de26efb010c1cae2b0e75eb2ef3f47a068714d9a
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-25 22:20:54 +0530

    ko

--- math lab/words.py
+++ math lab/words.py
@@ -2,7 +2,7 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-25 22:19:03
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-25 22:20:33
+# @Last Modified time: 2020-10-25 22:20:50
 
 
 
@@ -11,3 +11,4 @@ handle = open(name, 'r')
 counts = dict()
 
 for line in handle:
+        words = line.split()

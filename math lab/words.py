093bcc2a4b8cc306fae5116d62f783104a8e4b10
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-25 22:20:38 +0530

    op

--- math lab/words.py
+++ math lab/words.py
@@ -2,10 +2,12 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-25 22:19:03
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-25 22:20:16
+# @Last Modified time: 2020-10-25 22:20:33
 
 
 
 name = input('Enter file:')
 handle = open(name, 'r')
 counts = dict()
+
+for line in handle:

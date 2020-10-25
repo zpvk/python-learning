a103c45e92407240fd2fcd4a7127352e1d9b5604
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-25 22:20:05 +0530

    56

--- math lab/words.py
+++ math lab/words.py
@@ -2,8 +2,9 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-25 22:19:03
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-25 22:19:46
+# @Last Modified time: 2020-10-25 22:20:01
 
 
 
 name = input('Enter file:')
+handle = open(name, 'r')

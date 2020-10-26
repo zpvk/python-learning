74b5b7a1b99cd869fbec114c6b3ee66c99b3f398
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-26 23:43:43 +0530

    ec

--- cul2.py
+++ cul2.py
@@ -2,5 +2,6 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-26 23:43:23
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-26 23:43:25
+# @Last Modified time: 2020-10-26 23:43:38
 
+import urllib.request, urllib.parse, urllib.error

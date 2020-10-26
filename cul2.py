abaac364bb005e8e17335887317ad87bfb904f73
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-26 23:43:54 +0530

    rr

--- cul2.py
+++ cul2.py
@@ -2,6 +2,8 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-26 23:43:23
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-26 23:43:38
+# @Last Modified time: 2020-10-26 23:43:50
 
 import urllib.request, urllib.parse, urllib.error
+
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')

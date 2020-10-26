d167b12b1fce52a410002add75fafb81da1393c7
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-26 23:44:23 +0530

    cev

--- cul2.py
+++ cul2.py
@@ -2,9 +2,11 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-26 23:43:23
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-26 23:44:01
+# @Last Modified time: 2020-10-26 23:44:20
 
 import urllib.request, urllib.parse, urllib.error
 
 img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')
 fhand = open('cover3.jpg', 'wb')
+size = 0
+while True:

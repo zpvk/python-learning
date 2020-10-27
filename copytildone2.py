b91c8918d50a86cd920347cdb359ec020b368f32
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-10-27 23:58:56 +0530

    jj;

--- copytildone2.py
+++ copytildone2.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Rohan Kumara
+# @Date:   2020-10-27 23:58:50
+# @Last Modified by:   Rohan Kumara
+# @Last Modified time: 2020-10-27 23:58:50
+
+
 while True:
     line = input('> ')
     if line[0] == '#':

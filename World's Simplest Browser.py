cfc5b7a035f0d9cdb0f68386d5061616e1deceb4
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-11-03 23:51:45 +0530

    browser1

--- World's Simplest Browser.py
+++ World's Simplest Browser.py
@@ -2,8 +2,10 @@
 # @Author: Rohan Kumara
 # @Date:   2020-11-03 23:47:55
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-11-03 23:48:13
+# @Last Modified time: 2020-11-03 23:51:39
 
 
 import socket
 
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))

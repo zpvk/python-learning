8ce83a7130433c13efbecf857efb157782edbf35
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-11-03 23:56:38 +0530

    browser3

--- World's Simplest Browser.py
+++ World's Simplest Browser.py
@@ -2,13 +2,20 @@
 # @Author: Rohan Kumara
 # @Date:   2020-11-03 23:47:55
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-11-03 23:54:43
+# @Last Modified time: 2020-11-03 23:56:31
 
 
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org',80))
+mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+mysock.close()

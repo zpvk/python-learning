f75b3f910c04a30b035f0c39f222de349657c231
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-11-03 23:50:00 +0530

    browser1

---
+++ World's Simplest Browser.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Rohan Kumara
+# @Date:   2020-11-03 23:47:55
+# @Last Modified by:   Rohan Kumara
+# @Last Modified time: 2020-11-03 23:48:13
+
+
+import socket
+

59c7dcef4ea64c48a84fa89a208b9332e8c9905b
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-11-26 23:57:03 +0530

    t5

--- copytildone2.py
+++ copytildone2.py
@@ -2,14 +2,14 @@
 # @Author: Rohan Kumara
 # @Date:   2020-10-27 23:58:50
 # @Last Modified by:   Rohan Kumara
-# @Last Modified time: 2020-10-27 23:58:50
+# @Last Modified time: 2020-11-26 23:56:56
 
 
 while True:
     line = input('> ')
     if line[0] == '#':
         continue
-    if line == 'done':
+    if line == 'done with ':
         break
     print(line)
 print('Done!')

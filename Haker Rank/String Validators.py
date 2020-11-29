367289a7c9decff3fb8a1c78e01bd043933791e3
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2020-11-29 23:49:49 +0530

    string validators

---
+++ Haker Rank/String Validators.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Rohan Kumara
+# @Date:   2020-11-29 23:49:27
+# @Last Modified by:   Rohan Kumara
+# @Last Modified time: 2020-11-29 23:49:27
+if __name__ == '__main__':
+    s = input()

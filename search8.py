59cd03522ee835d17d67ce3183ba4fd22ad2332f
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-12-16 23:09:23 +0530

    Update code

--- search8.py
+++ search8.py
@@ -1,8 +1,1 @@
-fhand = open('mbox-short.txt')
-count = 0
-for line in fhand:
-    words = line.split()
-    if len(words) > 0:
-        if words[0] != 'From':
-            continue
-        print(words[2])
+

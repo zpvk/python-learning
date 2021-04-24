f12c514812a12ca931fa2c00ecbb7047ac74ffdb
Rohan Kumara <hmrohankumara@gmail.com>
2021-04-24 14:15:43 +0530

    Update 02_01.py

--- opencv/Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_01 Begin/02_01.py
+++ opencv/Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch02/02_01 Begin/02_01.py
@@ -3,6 +3,8 @@ import cv2
 
 img = cv2.imread("opencv-logo.png")
 cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
-cv2.imshow("image",img)
+cv2.imshow("image", img)
 
 cv2.waitKey(0)
+
+cv2.imwrite("output.jpg", img)

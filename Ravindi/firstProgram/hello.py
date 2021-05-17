59ba1e1af0809c9844c2c5d6fc335416d9b3471f
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-05-18 00:17:15 +0530

    data type retreiving

--- Ravindi/firstProgram/hello.py
+++ Ravindi/firstProgram/hello.py
@@ -4,3 +4,7 @@ print("Hello Ravindi\n Welcome to Python Programming")
 #try a simple for loop 
 for x in "Ravindi" : 
     print (x)
+
+# to retrieve the data type of the variable try out the following method called 'type'
+a = 1
+print(type(a))

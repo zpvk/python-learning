75db54bf36be4924a89af18474b1903d3f9c0a10
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-05-18 00:33:24 +0530

    for loop 2

--- Ravindi/firstProgram/hello.py
+++ Ravindi/firstProgram/hello.py
@@ -1,9 +1,15 @@
 #This is my very first python program
-print("Hello Ravindi\n Welcome to Python Programming")
+# 'print' method use to show the message 
+print("Hello Ravindi\nWelcome to Python Programming")
 
 #try a simple for loop 
+# for the String value "Ravindi" all the letters print in the console from one by one downwards
 for x in "Ravindi" : 
     print (x)
+
+#same for loop for number
+for x in 768 :
+    print(x)
 
 # to retrieve the data type of the variable try out the following method called 'type'
 a = 1

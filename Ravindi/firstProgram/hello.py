3904116e5ec9b5ea315f313e6d796cd78a122b73
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-05-18 00:39:34 +0530

    assign multiple values to variables

--- Ravindi/firstProgram/hello.py
+++ Ravindi/firstProgram/hello.py
@@ -1,15 +1,20 @@
 #This is my very first python program
 # 'print' method use to show the message 
-print("Hello Ravindi\nWelcome to Python Programming")
+print("Hello Ravindi\nWelcome to Python Programming\n")
+
+# assign values for variables and print them seperatly try this
+x = 1
+a, b, c = "Ravindi", "Rohan", "Apple"
+
+print(a)
+print(b)
+print(c)
+
 
 #try a simple for loop 
 # for the String value "Ravindi" all the letters print in the console from one by one downwards
 for x in "Ravindi" : 
     print (x)
-
-#same for loop for number
-for x in 768 :
-    print(x)
 
 # to retrieve the data type of the variable try out the following method called 'type'
 a = 1

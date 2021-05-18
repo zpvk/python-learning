99d2ed9f51e3e05c716a13370e82b962daa9e05f
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-05-18 23:56:27 +0530

    if else

--- Ravindi/firstProgram/hello.py
+++ Ravindi/firstProgram/hello.py
@@ -1,45 +1,75 @@
-#This is my very first python program
-# 'print' method use to show the message 
-print("Hello Ravindi\nWelcome to Python Programming\n")
+# #This is my very first python program
+# # 'print' method use to show the message 
+# print("Hello Ravindi\nWelcome to Python Programming\n")
 
-# assign values for variables and print them seperatly try this
-x = 1
-a, b, c = "Ravindi", "Rohan", "Apple"
+# # assign values for variables and print them seperatly try this
+# x = 1
+# a, b, c = "Ravindi", "Rohan", "Apple"
 
-print(a)
-print(b)
-print(c)
-
-
-#try a simple for loop 
-# for the String value "Ravindi" all the letters print in the console from one by one downwards
-for x in "Ravindi" : 
-    print (x)
-
-# to retrieve the data type of the variable try out the following method called 'type'
-a = 1
-print(type(a))
-
-#if you need to print two or more variables in one line follow this method
-name = "Ravindi"
-age = 23
-print ("My name is {} and i'm {} old".format(name,age))
+# print(a)
+# print(b)
+# print(c)
 
 
-#taking data from outside
-name = str(input("What's your name?"))
-print(name)
+# #try a simple for loop 
+# # for the String value "Ravindi" all the letters print in the console from one by one downwards
+# for x in "Ravindi" : 
+#     print (x)
+
+# # to retrieve the data type of the variable try out the following method called 'type'
+# a = 1
+# print(type(a))
+
+# #if you need to print two or more variables in one line follow this method
+# name = "Ravindi"
+# age = 23
+# print ("My name is {} and i'm {} old".format(name,age))
 
 
-#simple addition calculator
-num1 = int(input("Enter 1st number: "))
-num2 = int(input("Enter 2st number: "))
-answer = num1+num2
-print(answer)
+# #taking data from outside
+# name = str(input("What's your name?"))
+# print(name)
 
 
-#print same thing alone a line
-print("\n{x}star{x}\n".format(x="*"*10))
+# #simple addition calculator
+# num1 = int(input("Enter 1st number: "))
+# num2 = int(input("Enter 2st number: "))
+# answer = num1+num2
+# print(answer)
 
+
+# #print same thing alone a line
+# print("\n{x}star{x}\n".format(x="*"*10))
+
+
+
+# 3 > 2 answer is True
+# 4 < 1 answer is False
+# 1 == 3 answer is False.... This checks if this is true or false
+
+# x = 5
+# y = 10
+
+# if(y==x):
+#     print("Yes")
+# elif(y>x):
+#     print("OK")
+# else:
+#     print("No")
+
+
+
+marks = int(input("Enter marks: "))
+
+if (marks<35):
+    print
+
+
+# home work 2 (calculator)
+# 01) input first digit
+# 02) show operators have
+# 02) input operator need
+# 03) input 2nd digit
+# 04) show answer
 
 
--- Ravindi/firstProgram/simple_cal.py
+++ Ravindi/firstProgram/simple_cal.py
@@ -7,6 +7,9 @@ mult = num1*num2
 div = num1/num2
 fldiv = num1//num2
 mod = num1%num2
+round = round(num1)
+absoulte = abs(num1)
+
 print("\n{} + {} = {}".format(num1,num2,sum))
 print("{} - {} = {}".format(num1,num2,sub))
 print("{} * {} = {}".format(num1,num2,mult))

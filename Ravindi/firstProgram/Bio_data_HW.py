5c26191a6a680ccd904de8fcc19976d4d5ca978a
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-05-18 15:35:53 +0530

    Bio data home work

---
+++ Ravindi/firstProgram/Bio_data_HW.py
@@ -0,0 +1,25 @@
+#homework : create your own bio
+
+name = "Annada Samarakoon"
+email = "abc123@gmail.com"
+contact = "+94764545896"
+DOB = "2050-02-31"
+age = 12
+title = "student"
+height = "7'1''"
+qualification = "high intelligence"
+hobby = "Opera, Ball dance, Singing Sri Lanka Matha"
+
+x = str(input("Hello dr, Please enter your name: "))
+print(x,"Welcome to My Bio data\n\n")
+print("Name: ", name, "\n")
+print("Email: ", email, "\n")
+print("Contact: ", contact, "\n")
+print("Date of Birth: ", DOB, "\n")
+print("Age: ", age, "\n")
+print("Title: ", title, "\n")
+print("Height: ", height, "\n")
+print("Qualification: ", qualification, "\n")
+print("Hobbies: ", hobby, "\n")
+
+print("Thank you very {}. You are an wonderfull idiot\nCome Agatin".format(x))
--- Ravindi/firstProgram/hello.py
+++ Ravindi/firstProgram/hello.py
@@ -19,3 +19,22 @@ for x in "Ravindi" :
 # to retrieve the data type of the variable try out the following method called 'type'
 a = 1
 print(type(a))
+
+#if you need to print two or more variables in one line follow this method
+name = "Ravindi"
+age = 23
+print ("My name is {} and i'm {} old".format(name,age))
+
+
+#taking data from outside
+name = str(input("What's your name?"))
+print(name)
+
+
+#simple addition calculator
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2st number: "))
+answer = num1+num2
+print(answer)
+
+

b1dc88b847926530a50960983ab62a5a6fabbb73
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2021-05-18 22:15:51 +0530

    HW-bio_data part 2

--- Ravindi/firstProgram/Bio_data_HW.py
+++ Ravindi/firstProgram/Bio_data_HW.py
@@ -1,17 +1,16 @@
 #homework : create your own bio
 
-name = "Annada Samarakoon"
-email = "abc123@gmail.com"
-contact = "+94764545896"
-DOB = "2050-02-31"
-age = 12
-title = "student"
-height = "7'1''"
-qualification = "high intelligence"
-hobby = "Singing Sri Lanka Matha"
+name = str(input("Hello dr, Please enter your name: "))
+email = str(input("Please enter your emial: "))
+contact = str(input("Please enter your contact: "))
+DOB = str(input("Please enter your date of birth: "))
+age = int(input("Please enter your age: "))
+title = str(input("Please enter your title: "))
+height = str(input("Please enter your height: "))
+qualification = str(input("Please enter your qualifications: "))
+hobby = str(input("Please enter your hobbies: "))
 
-x = str(input("Hello dr, Please enter your name: "))
-print(x,"Welcome to My Bio data\n\n")
+print(name,"Welcome to My Bio data\n\n")
 print("Name: ", name, "\n")
 print("Email: ", email, "\n")
 print("Contact: ", contact, "\n")
@@ -22,4 +21,5 @@ print("Height: ", height, "\n")
 print("Qualification: ", qualification, "\n")
 print("Hobbies: ", hobby, "\n")
 
-print("Thank you {}. You are a wonderful idiot\nCome Again".format(x))
+print("Thank you {}. You are a wonderful idiot\n".format(name
+))

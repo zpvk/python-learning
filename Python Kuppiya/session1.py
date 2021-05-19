7279f178f33248854d1735eae99fe83739f3c16a
Rohan Kumara <hmrohankumara@gmail.com>
2021-05-19 22:08:22 +0530

    Create session1.py

---
+++ Python Kuppiya/session1.py
@@ -0,0 +1,49 @@
+# # # this code for the Inputs
+# name = str(input("Enter Your Name : "))
+# age = str(input("Enter Your Age : "))
+# address = str(input("Enter Your Address : "))
+# batch = str(input("Enter Your Batch : "))
+# mobile = str(input("Enter Your Mobile Number : "))
+# vilage = str(input("Enter Your Village Name : "))
+
+# # # this code for the bio
+# print("\n{x} This is your Bio {x}\n".format(x = "*"*10))
+# print("Name : {}\nAge : {}\nAddress : {}\nBatch :{}\nMobile Number : {}\nVillage : {}\n\n{}"
+# .format(name,age,address,batch,mobile,vilage,"*"*38))
+
+# num1 = int(input("Enter 1st number: "))
+# num2 = int(input("Enter 2st number: "))
+# sum = num1+num2
+# sub = num1-num2
+# mult = num1*num2
+# div = num1/num2
+# fldiv = num1//num2
+# mod = num1%num2
+# print("\n{} + {} = {}".format(num1,num2,sum))
+# print("{} - {} = {}".format(num1,num2,sub))
+# print("{} * {} = {}".format(num1,num2,mult))
+# print("{} / {} = {}".format(num1,num2,div))
+# print("{} // {} = {}".format(num1,num2,fldiv))
+# print("{} % {} = {}\n".format(num1,num2,mod))
+# num1 = int(input("Enter 1 Number"))
+# num2 = int(input("Enter 2 Number"))
+
+# print(num1 + num2)
+# print (num1 - num2)
+
+# x = 5
+# y = 10
+
+# if(y==x):
+#     print("Yes")
+# else:
+#     print("No")
+
+# if(y==x):
+#     print("Yes")
+# elif(y>x):
+#     print("ok")
+# else:
+#     print("No")
+
+

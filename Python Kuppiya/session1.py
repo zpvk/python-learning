1d7a41211cf22687fbeb18f22952e5c4db060e0e
Rohan Kumara <hmrohankumara@gmail.com>
2021-09-08 08:53:05 +0530

    Update session1.py

--- Python Kuppiya/session1.py
+++ Python Kuppiya/session1.py
@@ -139,17 +139,134 @@
 # else:
 #     print("something went wrong. Try Again")
 
-print("{x} Pass Mark Genarator {x}\n".format(x="*"*20))
-score = int(input("Enter your Score : "))
-mark = "Undifine"
-if (score>= 0 or score<=100 ):
-    if(score < 35):
-        mark = "Fail"
-    elif(score >= 35 or score<= 45):
-        mark = "C"
+# print("{x} Pass Mark Genarator {x}\n".format(x="*"*20))
+# score = int(input("Enter your Score : "))
+# mark = "Undifine"
+# if (score>= 0 or score<=100 ):
+#     if(score < 35):
+# #         mark = "Fail"
+# #     elif(score >= 35 or score<= 45):
+# #         mark = "C"
 
-print("Your Pass Mark is : ",mark)
+# # print("Your Pass Mark is : ",mark)
+
+# name = "xchathiru"
+
+# # print(name.upper())
+# # print(name.lower())
+# # print(name.strip())
+# # print(name.isalpha())
+# # print(name.endswith('x'))
+
+# Name = "Sri Lanka's documented history spans 3,000 years, with evidence of prehistoric human settlements dating back at least 125,000 years.[12] It has a rich cultural heritage, and the first known Buddhist writings of Sri Lanka, the Pāli Canon, date back to the Fourth Buddhist council in 29 BCE.[13][14] Its geographic location and deep harbours made it of great strategic importance from the time of the ancient Silk Road through to the modern Maritime Silk Road.[15][16][17] Its location as a major trading hub made it known to both the Far East as well as to Europe from as far back as the Anuradhapura period. The country's trade in luxury goods and spices attracted traders of many nations, creating Sri Lanka's diverse population. During a period of great political crisis, the Portuguese, whose arrival in Sri Lanka was largely accidental, sought to control the island's maritime regions and its lucrative external trade. The Portuguese possessions were later taken over by the Dutch. The Dutch possessions were then taken by the British, who later extended their control over the whole island, colonising it from 1815 to 1948. A national movement for political independence arose in the early 20th century, and in 1948, Ceylon became a republic and adopted its current name in 1972. Sri Lanka's recent history has been marred by a 26-year civil war, which ended decisively when the Sri Lanka Armed Forces defeated the Liberation Tigers of Tamil Eelam in 2009.[18]Sri Lanka is a multinational state, home to diverse cultures, languages, and ethnicities. The Sinhalese form the majority of the nation's population; and the large minority of Tamils have also played an influential role in the island's history, while Moors, Burghers, Malays, Chinese, and the indigenous Vedda are also established groups.[19] It has had a long history of international engagement, as a founding member of the SAARC, and a member of the United Nations, the Commonwealth of Nations, the G77, and the Non-Aligned Movement. Sri Lanka is the sole South Asian country rated high on the Human Development Index, with the second highest per capita income in the region."
+# print(Name.count('a'))
+# print(Name.count('s'))
+# print(Name.count('e'))
+
+# text = "Ss"
+# age = 90
+# print(text.count("S"))
+
+# x = int(input("Enter Age"))
+# y = str(input("Enter Your Name"))
+# # str()
+
+# text = "I\'m putting this in \"quotes\" because I can."
+# print(text)
+
+# list 
+
+# empty list
+
+my_list = ['k',1,3,65,745,'hfifhe']
+
+# print(my_list[-1])
+
+# Numbers = list(range(1,10))
+
+# print(Numbers)
+
+# x = [list(range(90,100))]
+# print(x)
+
+# Name = my_list[-1]
+# print(Name[-1])
+
+# pythonStudent = ['Chathiru','Prabashi','Ravindi','Ravindi']
+
+# pythonStudent.append(['Pawani','Himasha','Neha','Sayuri','Malsha'])
+# pythonStudent.remove('Chathiru')
+
+# y = pythonStudent.index('Ravindi')
+
+# print(y)
+
+# numbers = [1,2,3,4,67,34,90,6,7,8,9,10]
+# print(numbers)
+# numbers.sort()
+# print(numbers)
+# numbers.reverse()
+# print(numbers)
+# numbers.pop()
+# print(numbers)
+
+# 1
+# 2
+# 3
+# 4
+# 6
+# 7
+# 8
+# 9
+# 10
+# # 34
+# # 67
+# # 90
+# # Stack and Queues
+# #Stack(LIFO)- Last in First Out
+
+# stack = [1,8,9]
+
+# stack.append(4)
+# print(stack)
+# print(stack.pop())
+# print(stack.pop())
+# print(stack)
 
 
+# book = {"Name":["Rohan","Kumara","Herath"]}
+# book["color"] = "Red"
+# print(book["Name"])
+# x=book.pop("Name")
+# print(x)
 
+
+my_list = ["Rohan","Bort","GUYJH","ebuyf",2354,"vdbv"]
+
+# i = 1
+# for x in my_list:
+#     print(i)
+#     i= i+i
+
+# for x in range(1,89):
+#     print(x)
+
+# No return Type No Perameters
+def add():
+    print(1+1)
+
+# No return type With Perameters
+
+def add1(x,y):
+    print(x+y)
+
+#Return Types witth No Para
+
+def add2():
+    return 1+1
+
+# return type with para
+
+def add3(x,y):
+    return x+y
 

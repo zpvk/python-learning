b3198d6626e4dc851259247f5eec6512d4e65f8f
Rohan Kumara <hmrohankumara@gmail.com>
2021-09-08 08:53:02 +0530

    Create Basic ATM.py

---
+++ Python Kuppiya/Basic ATM.py
@@ -0,0 +1,50 @@
+# print("{x} Welcome to BOC 1998 {x}\n".format(x = "*"*10))
+# password = 123
+# balance = 1000
+# wish = "\nThank You for Banking with Us\n"
+# input_password = int(input("Enter Your Password : "))
+# if (input_password == password):
+#     print("\n Welcome To BOC 1998 \n 1. Balance \n 2. Diposit \n 3. Widrow \n")
+#     Operation = int(input("Enter Your Choice : "))
+#     if Operation == 1:
+#         print("Your Current Balance is : ", balance)
+#         print(wish)
+#     elif Operation == 2:
+#         amount = int(input("Enter your Amount : "))
+#         balance += amount
+#         print("Now Your Current Ammounr is : ",balance)
+#         print(wish)
+#     elif Operation == 3:
+#         widrow = int(input("Enter your widrow ammount : "))
+#         if widrow > balance:
+#             print("\n Insufiction Balance please Try Again \n ",wish)
+#         else:
+#             balance -=widrow
+#             print("Your Current Balance is : ",balance)
+#             print(wish)
+# else:
+#     print("\n Your Password is incorect please try again Thank You")
+
+
+
+#pawani
+
+# BankCode = ("1234")
+# Blance=5000
+# y=int(input("Enter Your Pin Number"))
+
+# A= Deposit 
+# B= Widrow
+# C= Balance
+# if("Bank Code = y "):
+# print("Select Deposit , Widrow , Balance")
+# elif("A=Deposit"):
+#     print("Enter Your Amount")
+# elif("B = Widrow"):
+#     print("Enter Your Amount")
+# elif("C= Blance"):
+#     print("Enter your Account Number ")    
+# else("Bank Code != y"):
+# print("Your Pin Number Are Incorrect")
+
+#prabashi

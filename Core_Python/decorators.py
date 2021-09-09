214c84e2ed9dc7749a1de52022757b1b3f08930b
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2021-09-09 10:45:47 +0530

    Create decorators.py

---
+++ Core_Python/decorators.py
@@ -0,0 +1,10 @@
+def anncounce(f):
+    def wapper():
+        print("About to run th function")
+        f()
+        print("Done with the function")
+    return wapper
+
+@anncounce
+def hello():
+    print("Hello world")

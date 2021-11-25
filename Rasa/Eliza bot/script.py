c9fc1c4fcdb6542d5f4798f8f68d972e4580719f
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2021-11-25 12:08:29 +0530

    Create script.py

---
+++ Rasa/Eliza bot/script.py
@@ -0,0 +1,60 @@
+# Import the random module
+import random
+
+bot_template = 'BOT : {0}'
+user_template = 'USER : {0}'
+
+
+name = "Greg"
+weather = "cloudy"
+
+# Define a dictionary containing a list of responses for each message
+responses = {
+  "what's your name?": [
+      "my name is {0}".format(name),
+      "they call me {0}".format(name),
+      "I go by {0}".format(name)
+   ],
+  "what's today's weather?": [
+      "the weather is {0}".format(weather),
+      "it's {0} today".format(weather)
+    ],
+  "default": ["default message"]
+}
+
+# Use random.choice() to choose a matching response
+def respond(message):
+    # Check if the message is in the responses
+    if message in responses:
+        # Return a random matching response
+        bot_message = random.choice(responses[message])
+    else:
+        # Return a random "default" response
+        bot_message = random.choice(responses["default"])
+    return bot_message
+
+def respond(message):
+    # Check for a question mark
+    if message.endswith("?"):
+        # Return a random question
+        return random.choice(responses["statement"])
+    # Return a random statement
+    return random.choice(responses["question"])
+    # Define a function that sends a message to the bot: send_message
+
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
+
+# Send messages ending in a question mark
+send_message("what's today's weather?")
+send_message("what's today's weather?")
+
+# Send messages which don't end with a question mark
+send_message("I love building chatbots")
+send_message("I love building chatbots")
